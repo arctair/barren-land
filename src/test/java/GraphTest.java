0e5f3156b22bf292e8edf09e294c9de0ed3bf183
arctair <jtyler_@outlook.com>
2018-09-30 19:27:18 -0500

    remove references to Rectangle in GraphTest

--- src/test/java/GraphTest.java
+++ src/test/java/GraphTest.java
@@ -6,11 +6,11 @@ import static junit.framework.TestCase.assertTrue;
 
 public class GraphTest {
 
-	private void assertGraphsEquals(List<Set<Rectangle>> expected, List<Set<Rectangle>> actual) {
-		for (Set<Rectangle> subgraph_expected : expected) {
+	private void assertGraphsEquals(List<Set<Object>> expected, List<Set<Object>> actual) {
+		for (Set<Object> subgraph_expected : expected) {
 			assertTrue(String.format("Actual list %s did not contain %s", actual, subgraph_expected), actual.contains(subgraph_expected));
 		}
-		for (Set<Rectangle> subgraph_actual : actual) {
+		for (Set<Object> subgraph_actual : actual) {
 			assertTrue(String.format("Expected list %s did not contain %s", expected, subgraph_actual), expected.contains(subgraph_actual));
 		}
 	}
@@ -24,23 +24,23 @@ public class GraphTest {
 		| r3 | | r4 |
 		+----+-+----+
 		 */
-		Rectangle r1 = new Rectangle(60, 50, 100, 100);
-		Rectangle r2 = new Rectangle(0, 50, 40, 100);
-		Rectangle r3 = new Rectangle(0, 0, 40, 50);
-		Rectangle r4 = new Rectangle(60, 0, 100, 50);
+		Object r1 = new Object();
+		Object r2 = new Object();
+		Object r3 = new Object();
+		Object r4 = new Object();
 
-		List<Set<Rectangle>> expected = new ArrayList<Set<Rectangle>>() {{
-			add(new HashSet<Rectangle>() {{
+		List<Set<Object>> expected = new ArrayList<Set<Object>>() {{
+			add(new HashSet<Object>() {{
 				add(r1);
 				add(r4);
 			}});
-			add(new HashSet<Rectangle>() {{
+			add(new HashSet<Object>() {{
 				add(r2);
 				add(r3);
 			}});
 		}};
 
-		Graph<Rectangle> graph = new Graph<>();
+		Graph<Object> graph = new Graph<>();
 		graph.addNode(r1);
 		graph.addNode(r2);
 		graph.addNode(r3);
@@ -48,7 +48,7 @@ public class GraphTest {
 		graph.addEdge(r1, r4);
 		graph.addEdge(r2, r3);
 
-		List<Set<Rectangle>> actual = graph.disconnectedSubgraphs();
+		List<Set<Object>> actual = graph.disconnectedSubgraphs();
 		assertGraphsEquals(expected, actual);
 	}
 
@@ -69,24 +69,24 @@ public class GraphTest {
 		|     | r1  | r2  | r3  | r4  | r5  |     |
 		+-----+-----+-----+-----+-----+-----+-----+
 		 */
-		Rectangle r0 = new Rectangle(0, 0, 48, 600);
-		Rectangle r1 = new Rectangle(48, 0, 120, 192);
-		Rectangle r2 = new Rectangle(120, 0, 136, 52);
-		Rectangle r3 = new Rectangle(136, 0, 260, 192);
-		Rectangle r4 = new Rectangle(260, 0, 276, 52);
-		Rectangle r5 = new Rectangle(276, 0, 352, 192);
-		Rectangle r6 = new Rectangle(352, 0, 400, 600);
-		Rectangle r7 = new Rectangle(48, 208, 120, 392);
-		Rectangle r8 = new Rectangle(136, 208, 260, 392);
-		Rectangle r9 = new Rectangle(276, 208, 352, 392);
-		Rectangle r10 = new Rectangle(48, 408, 120, 600);
-		Rectangle r11 = new Rectangle(136, 408, 260, 600);
-		Rectangle r12 = new Rectangle(276, 408, 352, 600);
-		Rectangle r13 = new Rectangle(260, 548, 276, 600);
-		Rectangle r14 = new Rectangle(120, 548, 136, 600);
+		Object r0 = new Object();
+		Object r1 = new Object();
+		Object r2 = new Object();
+		Object r3 = new Object();
+		Object r4 = new Object();
+		Object r5 = new Object();
+		Object r6 = new Object();
+		Object r7 = new Object();
+		Object r8 = new Object();
+		Object r9 = new Object();
+		Object r10 = new Object();
+		Object r11 = new Object();
+		Object r12 = new Object();
+		Object r13 = new Object();
+		Object r14 = new Object();
 
-		List<Set<Rectangle>> expected = new ArrayList<Set<Rectangle>>() {{
-			add(new HashSet<Rectangle>() {{
+		List<Set<Object>> expected = new ArrayList<Set<Object>>() {{
+			add(new HashSet<Object>() {{
 				add(r0);
 				add(r1);
 				add(r2);
@@ -102,12 +102,12 @@ public class GraphTest {
 				add(r13);
 				add(r14);
 			}});
-			add(new HashSet<Rectangle>() {{
+			add(new HashSet<Object>() {{
 				add(r8);
 			}});
 		}};
 
-		Graph<Rectangle> graph = new Graph<>();
+		Graph<Object> graph = new Graph<>();
 		graph.addNode(r0);
 		graph.addNode(r1);
 		graph.addNode(r2);
@@ -138,7 +138,7 @@ public class GraphTest {
 		graph.addEdge(r11, r14);
 		graph.addEdge(r12, r13);
 
-		List<Set<Rectangle>> actual = graph.disconnectedSubgraphs();
+		List<Set<Object>> actual = graph.disconnectedSubgraphs();
 		assertGraphsEquals(expected, actual);
 	}
 
